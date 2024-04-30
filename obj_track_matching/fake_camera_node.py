a7cd73c1dfa004f11e08b63afd26185de4cd0ac0
Catherine Glossop <glossopcatherine@gmail.com>
2024-04-30 01:26:23 -0700

    gt

--- obj_track_matching/fake_camera_node.py
+++ obj_track_matching/fake_camera_node.py
@@ -87,7 +87,7 @@ def main():
             fake_camera_node.seg_count = 0
         fake_camera_node.image_msg = fake_camera_node.bridge.cv2_to_imgmsg(fake_camera_node.input_frames[fake_camera_node.frame_count%len(fake_camera_node.input_frames)], "passthrough")
         fake_camera_node.input_pub.publish(fake_camera_node.image_msg)
-        fake_camera_node.gt_msg = fake_camera_node.bridge.cv2_to_imgmsg(fake_camera_node.seg_frames[fake_camera_node.seg_count], "passthrough")
+        fake_camera_node.gt_msg = fake_camera_node.bridge.cv2_to_imgmsg(fake_camera_node.seg_frames[fake_camera_node.frame_count%len(fake_camera_node.input_frames)], "passthrough")
         fake_camera_node.gt_pub.publish()
         if fake_camera_node.frame_count % fake_camera_node.seg_period == 0:
             fake_camera_node.seg_msg = fake_camera_node.bridge.cv2_to_imgmsg(fake_camera_node.seg_frames[fake_camera_node.seg_count], "passthrough")
