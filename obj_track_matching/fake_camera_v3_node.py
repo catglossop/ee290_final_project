70f79304ce5a4dd5b7759ef23ff993b85583a40a
Catherine Glossop <glossopcatherine@gmail.com>
2024-05-07 20:47:19 -0700

    adding cli

--- obj_track_matching/fake_camera_v3_node.py
+++ obj_track_matching/fake_camera_v3_node.py
@@ -13,11 +13,12 @@ from cv_bridge import CvBridge # INSTALL ON PI
 
 class FakeCameraV3Node:
 
-    def __init__(self):
+    def __init__(self, video_path, test_label, fps):
 
         # GET TEST DATA
-        video_path = "/home/roboslice/eval_vid_0.mp4"
-        test_label = 0
+        self.video_path = video_path
+        self.test_label = test_label
+        self.fps = fps
 
         self.input_frames = input_frames
         self.frame_count = 0
@@ -45,11 +46,11 @@ class FakeCameraV3Node:
         self.iter_count = 0
 
 
-def main():
+def main(args):
 
     rospy.init_node('fake_camera_node', anonymous=True)
-    fake_camera_node = FakeCameraNode()
-    rate = rospy.Rate(60) # 60Hz
+    fake_camera_node = FakeCameraV3Node(video_path=args.video_path, test_label=args.test_label, fps=args.fps)
+    rate = rospy.Rate(fake_camera_node.fps)) # 60Hz
 
 
     while not rospy.is_shutdown():
@@ -62,7 +63,12 @@ def main():
     
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Fake Camera Node')
+    args = parser.parse_args()
+    parser.add_argument('--video_path', type=str, default='/home/roboslice/ee290/eval_vid_0.avi')
+    parser.add_argument('--test_label', type=int, default=0)
+    parser.add_argument('--fps', type=int, default=60)
+    main(args)
 
 
     
