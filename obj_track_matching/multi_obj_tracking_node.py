aa3a4b3cf58da25d1e579ff7ba0f5f8f43fae561
Catherine Glossop <glossopcatherine@gmail.com>
2024-05-07 19:07:37 -0700

    adding updates to multi obj tracking

--- obj_track_matching/multi_obj_tracking_node.py
+++ obj_track_matching/multi_obj_tracking_node.py
@@ -52,10 +52,42 @@ class MultiObjectTrackingNode:
         self.ious_gt_per_fps = []
         self.loop_time_per_fps = []
         self.loop_clock_per_fps = []
+        self.centroid_diff = {}
         self.periods = [10, 20, 30, 40, 50, 60, 70]
 
         for f in glob.glob("output/perf_eval_*.jpg"):
             os.remove(f)
+
+    def resize_polygon(self, poly):
+        poly_new = poly.copy()
+        centroid = np.mean(poly_new, axis=0)
+
+        # scale the points
+        poly_new = ((poly_new - centroid) * self.scale + centroid).astype(np.int64)
+        # visualize
+        if self.VISUALIZE:
+            img = self.curr_frame.copy()
+            img = cv.polylines(img, [np.flip(poly_new, axis=2)], True, (255,255,255), 1)
+            img = cv.polylines(img, [np.flip(poly.reshape(-1, 1, 2), axis=2)], True, (255,255,255), 1)
+            cv.imshow("seg region", img)
+            cv.waitKey(0)
+            cv.destroyAllWindows()
+
+        return poly_new
+    
+    def get_polygon(self, mask):
+        arg_mask = np.argwhere(mask == 255)
+        Px_max = arg_mask[np.argmax(arg_mask[:,0]), :]
+        Px_min = arg_mask[np.argmin(arg_mask[:,0]), :]
+        Py_max = arg_mask[np.argmax(arg_mask[:,1]), :]
+        Py_min = arg_mask[np.argmin(arg_mask[:,1]), :]
+
+
+        # Turn the points into an array
+        seg_pts = np.array([Px_min, Py_max, Px_max, Py_min])
+        seg_pts = seg_pts.reshape(-1, 1, 2)
+
+        return seg_pts
 
     def seg_callback(self, msg):
         self.prev_seg = self.curr_seg
@@ -161,13 +193,20 @@ class MultiObjectTrackingNode:
 
             ious_gt, ious = [], []
             for nseg in range(1, self.num_segs+1):
-                (prev_kps, prev_descs) = self.prev_kps_descs[nseg]
-                matches = self.matcher.match(self.prev_kps_descs[nseg][1], curr_descs)
-                viz_matches = sorted(matches, key=lambda x: x.distance)[:10]
-                matches = sorted(matches, key=lambda x: x.distance)
 
                 n_init_seg_pts = self.init_seg_pts[nseg]
                 n_seg_pts = self.seg_pts[nseg]
+
+                resized_mask = np.zeros_like(self.curr_seg)
+                resized_seg_pts = self.resize_polygon(n_seg_pts)
+                resized_mask = cv.fillPoly(resized_mask, [np.flip(resized_seg_pts, axis=2)], 255)
+
+                curr_kps, curr_descs = self.orb.detectAndCompute(self.curr_frame, resized_mask) 
+                (prev_kps, prev_descs) = self.prev_kps_descs[nseg]
+
+                matches = self.matcher.match(self.prev_kps_descs[nseg][1], curr_descs)
+                # viz_matches = sorted(matches, key=lambda x: x.distance)[:10]
+                matches = sorted(matches, key=lambda x: x.distance)
 
                 if self.VISUALIZE:
                     self.viz_img = cv.drawMatches(self.prev_frame, self.prev_kps_descs[nseg][0], self.curr_frame, curr_kps, viz_matches, None) 
@@ -180,21 +219,24 @@ class MultiObjectTrackingNode:
                     self.annotate_pub.publish(self.viz_msg)
 
                 # Find the set of points in the segmentation zone with good correspondences
-                u_pts = []
-                v_pts = []
-                seg_curr_kps = ()
-                seg_curr_descs = np.array([], dtype=np.uint8).reshape(0, 32)
-                for match in matches:
-                    if curr_kps[match.trainIdx].pt[0] < n_seg_pts[3,0,1] or curr_kps[match.trainIdx].pt[0] > n_seg_pts[1,0,1] or curr_kps[match.trainIdx].pt[1] < n_seg_pts[0,0,0] or curr_kps[match.trainIdx].pt[1] > n_seg_pts[2,0,0]:
-                        continue 
-                    else:
-                        u_pts.append(prev_kps[match.queryIdx].pt)
-                        v_pts.append(curr_kps[match.trainIdx].pt)
-                        seg_curr_kps = seg_curr_kps + (curr_kps[match.trainIdx],)
-                        seg_curr_descs = np.vstack((seg_curr_descs, curr_descs[match.trainIdx]))
+                # u_pts = []
+                # v_pts = []
+                # seg_curr_kps = ()
+                # seg_curr_descs = np.array([], dtype=np.uint8).reshape(0, 32)
+                # for match in matches:
+                #     if curr_kps[match.trainIdx].pt[0] < n_seg_pts[3,0,1] or curr_kps[match.trainIdx].pt[0] > n_seg_pts[1,0,1] or curr_kps[match.trainIdx].pt[1] < n_seg_pts[0,0,0] or curr_kps[match.trainIdx].pt[1] > n_seg_pts[2,0,0]:
+                #         continue 
+                #     else:
+                #         u_pts.append(prev_kps[match.queryIdx].pt)
+                #         v_pts.append(curr_kps[match.trainIdx].pt)
+                #         seg_curr_kps = seg_curr_kps + (curr_kps[match.trainIdx],)
+                #         seg_curr_descs = np.vstack((seg_curr_descs, curr_descs[match.trainIdx]))
 
-                u_pts = np.flip(np.float32(u_pts).reshape(-1, 1, 2), axis=2)
-                v_pts = np.flip(np.float32(v_pts).reshape(-1, 1, 2), axis=2)
+                # u_pts = np.flip(np.float32(u_pts).reshape(-1, 1, 2), axis=2)
+                # v_pts = np.flip(np.float32(v_pts).reshape(-1, 1, 2), axis=2)
+
+                u_pts = np.flip(np.float32([prev_kps[match.queryIdx].pt for match in matches]).reshape(-1, 1, 2), axis=2)
+                v_pts = np.flip(np.float32([curr_kps[match.trainIdx].pt for match in matches]).reshape(-1, 1, 2), axis=2)
             
                 # Get the homography defined by this set of points
                 try: 
@@ -211,6 +253,8 @@ class MultiObjectTrackingNode:
 
                 # Measure delta between the previous and current seg pts 
                 delta = n_seg_pts - n_prev_seg_pts
+                delta_vel = np.mean(delta, axis=0)
+                self.scale = np.abs(delta_vel/self.seg_pts[nseg].mean(axis=0)).mean() + 1.0
 
                 # Get IOU weight
                 self.prev_mask = cv.fillPoly(np.zeros_like(self.curr_seg), [np.flip(n_prev_seg_pts.astype(np.int64), axis=2)], 255)
@@ -228,6 +272,18 @@ class MultiObjectTrackingNode:
                 self.gt_mask = np.where(self.gt_seg==nseg, 255, 0)
                 iou_gt = np.sum(np.logical_and(self.gt_mask, self.curr_mask)) / np.sum(np.logical_or(self.gt_mask, self.curr_mask))
                 ious_gt.append(iou_gt)
+
+                # Get centroid difference with GT segmentation
+                gt_mask = np.where(self.seg_frames[self.frame_idx]==nseg, 255, 0)
+                gt_seg_pts = self.get_polygon(gt_mask)
+                gt_centroid = np.mean(gt_seg_pts, axis=0)
+                curr_centroid = np.mean(n_seg_pts, axis=0)
+
+                if self.centroid_diff.get(nseg) is None:
+                    self.centroid_diff[nseg] = []
+                    self.centroid_diff[nseg].append(np.linalg.norm(gt_centroid - curr_centroid))
+                else:
+                    self.centroid_diff[nseg].append(np.linalg.norm(gt_centroid - curr_centroid))
 
                 self.curr_mask_out = cv.fillPoly(self.curr_mask_out, [np.flip(n_seg_pts, axis=2)], int(nseg*(255//self.num_segs)))
             
